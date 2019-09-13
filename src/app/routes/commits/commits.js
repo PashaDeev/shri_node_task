f635f6ed3678640aae75ab48c82bd9f1bdd04334
Deev Pavel <deev@uchi.ru>
2019-09-13 22:24:20 +0300

    get commits rout

--- src/app/routes/commits/commits.js
+++ src/app/routes/commits/commits.js
@@ -6,14 +6,25 @@ const { err: errorDebug } = require(`../../debug`);
 async function getCommits(directory, hash) {
   let res;
   try {
-    res = await execFile(`git`, [`rev-list`, hash], { cwd: `${directory}.git` });
+    res = await execFile(`git`, [`log`,  `--pretty=format:%H|%ad|%s`, `--date=short`, hash], { cwd: `${directory}.git` });
   } catch (err) {
     if (err.code === `ENOENT`) {
       errorDebug(`no directory`);
       return `no directory`;
     }
+    console.log(`-------------------------`);
+    console.log(`err`, err);
+    console.log(`-------------------------`);
+    return `err`;
   }
-  return res.stdout.trim().split(`\n`);
+  return res.stdout.trim().split(`\n`).map((commit) => {
+    const [commitHash, date, name] = commit.split(`|`);
+    return {
+      hash: commitHash,
+      date,
+      commitMsg: name
+    }
+  });
 }
 
 module.exports = getCommits;

9a08ebef12038f013ced2435c440f4332aeed998
Deev Pavel <deev@uchi.ru>
2019-09-13 16:26:25 +0300

    add main rout

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const fs = require(`fs-extra`);
+const path = require(`path`);
 const { router } = require(`./routes`);
 
 const app = express();
 
-app.get(`/`, (req, res) => {
-  res.end(`hello`);
+app.get(`/`, async (req, res) => {
+  const fileData = await fs.readFile(path.join(__dirname, `views/main/index.html`));
+  res.end(fileData);
 });
 
 app.get(`/`, (req, res) => {
--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -1,13 +1,18 @@
 const express = require(`express`);
+const debug = require(`debug`)(`router: `)
 const router = express.Router();
 const { main } = require(`./main`);
 
 function mainRout() {
   router.get(`/`, main);
 
-  router.get(`/:repositoryId/commits/:commitHash`, (req, res) => {
+  router.get(`/:repositoryId/commits/:commitHash`, async (req, res) => {
+    debug(`commit start`);
     const {repositoryId, commitHash} = req.params;
-    return res.end(`${repositoryId} ${commitHash}`);
+    res.write(`fist part ${new Date()} \n`);
+    await new Promise(res => setTimeout(() => res(), 1000))
+    res.end(`end part ${repositoryId} ${commitHash} ${new Date()}`);
+    debug(`commit end`);
   });
 
   return router;

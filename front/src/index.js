0ab82e07b227041fcb8d8bc85ba95bc1bf026e79
Deev Pavel <deev@uchi.ru>
2019-09-21 17:48:43 +0300

    history

--- front/src/index.js
+++ front/src/index.js
@@ -2,13 +2,13 @@ const fileTab = document.body.querySelector(`.first`);
 const branchTab = document.body.querySelector(`.second`);
 
 const fileList = document.body.querySelector(`.file`);
-// const branchList = document.body.querySelector(`.repository__branch-list`);
+const branchList = document.body.querySelector(`.history`);
 
-fileList.classList.remove(`section_visibility_invisible`);
-fileTab.classList.add(`section_border-color_red`);
-fileTab.classList.remove(`text_view_ghost`);
-fileTab.classList.add(`repository__tab-item_state_current`);
-// branchList.classList.add(`repository__list_status_invisible`);
+branchList.classList.remove(`section_visibility_invisible`);
+branchTab.classList.add(`section_border-color_red`);
+branchTab.classList.remove(`text_view_ghost`);
+branchTab.classList.add(`repository__tab-item_state_current`);
+fileList.classList.add(`section_visibility_invisible`);
 
 fileTab.addEventListener(`click`, () => {
   fileList.classList.remove(`section_visibility_invisible`);
@@ -16,14 +16,14 @@ fileTab.addEventListener(`click`, () => {
   fileTab.classList.remove(`text_view_ghost`);
   fileTab.classList.add(`repository__tab-item_state_current`);
 
-  // branchList.classList.remove(`section_visibility_invisible`);
+  branchList.classList.remove(`section_visibility_invisible`);
   branchTab.classList.remove(`section_border-color_red`);
   branchTab.classList.add(`text_view_ghost`);
   branchTab.classList.remove(`repository__tab-item_state_current`);
 });
 
 branchTab.addEventListener(`click`, () => {
-  // branchList.classList.remove(`repository__list_status_invisible`);
+  branchList.classList.remove(`section_visibility_invisible`);
   branchTab.classList.add(`section_border-color_red`);
   branchTab.classList.remove(`text_view_ghost`);
   branchTab.classList.add(`repository__tab-item_state_current`);

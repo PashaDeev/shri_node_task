a05438ba4b478cf2cac4039e8ffc07134b32e394
Deev Pavel <deev@uchi.ru>
2019-09-21 16:56:24 +0300

    add file details section

--- front/src/index.js
+++ front/src/index.js
@@ -1,34 +1,34 @@
-const fileTab = document.body.querySelector(`.repository__tab-item_type_files`);
-const branchTab = document.body.querySelector(`.repository__tab-item_type_branches`);
+const fileTab = document.body.querySelector(`.first`);
+const branchTab = document.body.querySelector(`.second`);
 
-const fileList = document.body.querySelector(`.repository__dir-list`);
-const branchList = document.body.querySelector(`.repository__branch-list`);
+const fileList = document.body.querySelector(`.file`);
+// const branchList = document.body.querySelector(`.repository__branch-list`);
 
-fileList.classList.remove(`repository__list_status_invisible`);
+fileList.classList.remove(`section_visibility_invisible`);
 fileTab.classList.add(`section_border-color_red`);
 fileTab.classList.remove(`text_view_ghost`);
 fileTab.classList.add(`repository__tab-item_state_current`);
-branchList.classList.add(`repository__list_status_invisible`);
+// branchList.classList.add(`repository__list_status_invisible`);
 
 fileTab.addEventListener(`click`, () => {
-  fileList.classList.remove(`repository__list_status_invisible`);
+  fileList.classList.remove(`section_visibility_invisible`);
   fileTab.classList.add(`section_border-color_red`);
   fileTab.classList.remove(`text_view_ghost`);
   fileTab.classList.add(`repository__tab-item_state_current`);
 
-  branchList.classList.add(`repository__list_status_invisible`);
+  // branchList.classList.remove(`section_visibility_invisible`);
   branchTab.classList.remove(`section_border-color_red`);
   branchTab.classList.add(`text_view_ghost`);
   branchTab.classList.remove(`repository__tab-item_state_current`);
 });
 
 branchTab.addEventListener(`click`, () => {
-  branchList.classList.remove(`repository__list_status_invisible`);
+  // branchList.classList.remove(`repository__list_status_invisible`);
   branchTab.classList.add(`section_border-color_red`);
   branchTab.classList.remove(`text_view_ghost`);
   branchTab.classList.add(`repository__tab-item_state_current`);
 
-  fileList.classList.add(`repository__list_status_invisible`);
+  fileList.classList.add(`section_visibility_invisible`);
   fileTab.classList.remove(`section_border-color_red`);
   fileTab.classList.add(`text_view_ghost`);
   fileTab.classList.remove(`repository__tab-item_state_current`);
